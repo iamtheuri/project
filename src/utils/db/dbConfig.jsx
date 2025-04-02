7bf0fcd6e14a4b6ae2c4b3b7f04071b2c8b39322
iamtheuri <theuridavid56@gmail.com>
2025-04-03 01:43:54 +0300

    Stop tracking config files

--- src/utils/db/dbConfig.jsx
+++
@@ -1,6 +0,0 @@
-import { drizzle } from "drizzle-orm/neon-http";
-import { neon } from "@neondatabase/serverless";
-import * as schema from "./schema";
-// Had to do away with initial db url and delete it as I exposed it mistakenly while testing, but all security concerns now sorted
-const sql = neon("postgresql://neondb_owner:npg_6cVytOTf3ZQB@ep-royal-thunder-a4ar37nx-pooler.us-east-1.aws.neon.tech/trash2token?sslmode=require");
-export const db = drizzle(sql, { schema });
