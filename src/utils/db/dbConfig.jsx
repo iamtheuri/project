273fbd718e627e40c810b798f360b96360db96ef
iamtheuri <theuridavid56@gmail.com>
2025-04-02 22:51:28 +0300

    Had to do away with initial db url and delete it as I exposed it mistakenly while testing, but all security concerns now sorted

--- src/utils/db/dbConfig.jsx
+++ src/utils/db/dbConfig.jsx
@@ -1,5 +1,6 @@
 import { drizzle } from "drizzle-orm/neon-http";
 import { neon } from "@neondatabase/serverless";
 import * as schema from "./schema";
+// Had to do away with initial db url and delete it as I exposed it mistakenly while testing, but all security concerns now sorted
 const sql = neon(process.env.DB_URL);
 export const db = drizzle(sql, { schema });
