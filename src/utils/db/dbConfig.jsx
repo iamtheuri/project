fb48b2ce525003a7c78c6f7c53039ca183aea4e4
iamtheuri <theuridavid56@gmail.com>
2025-04-02 22:21:51 +0300

    Delete src/utils/db/dbConfig.jsx

--- src/utils/db/dbConfig.jsx
+++
@@ -1,5 +0,0 @@
-import { drizzle } from "drizzle-orm/neon-http";
-import { neon } from "@neondatabase/serverless";
-import * as schema from "./schema";
-const sql = neon("postgresql://neondb_owner:npg_6cVytOTf3ZQB@ep-royal-thunder-a4ar37nx-pooler.us-east-1.aws.neon.tech/neondb?sslmode=require");
-export const db = drizzle(sql, { schema });
